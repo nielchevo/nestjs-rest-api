cb6bcfcf8ab1f0f8a2ef0bf78be9b33d0b16cdd5
Nathaniel Gunawan <nielchvo@gmail.com>
2020-03-03 16:52:20 +0700

    add user entity

---
+++ src/users/users.entity.ts
@@ -0,0 +1,38 @@
+import { Model, Table, Column, TableOptions, DataType } from 'sequelize-typescript';
+import { DataType, DataType } from 'sequelize/types';
+
+// Tabel options config
+const tableOption: TableOptions = { timestamps: true }; 
+
+@Table(tableOption)
+export class Users extends Model<Users> {
+    @Column({ 
+        type: DataType.INTEGER, 
+        autoIncrement: true,
+        primaryKey: true,
+        unique: true
+    })
+    idUser: number;
+
+    @Column
+    first_name: string;
+    
+    @Column
+    last_name: string;
+    
+    @Column
+    phone: string;
+
+    @Column
+    email: string;
+
+    @Column
+    createdAt: Date;
+
+    @Column
+    updatedAt: Date;
+
+    @Column
+    deletedAt: Date;
+};
+
